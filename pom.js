7078828ab5153e3a452e4beb52945a2c7bc3ae57
Nicolas Baudoin <baudoin.nicolasg@gmail.com>
2022-06-20 15:50:13 +0200

    premier essaie de code js échoué, ajout d'un style sur valider

--- pom.js
+++ pom.js
@@ -3,6 +3,20 @@ function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
-let mystery = randomNumber(1, 100);
+const result = document.querySelector("#reponse");
+const mystery = randomNumber(1, 100);
+const guess = document.querySelector("#guess"); //trouver comment récupérer la value de textContent #guess
 
 console.log(mystery);
+
+valider.addEventListener("click", () => {
+  if (this.guess.value === mystery) {
+    result.textContent = "Gagné !";
+  } else if (this.guess.value < mystery) {
+    result.textCeontent = "Raté ! C'est plus grand !";
+  } else {
+    result.textContent = "Raté ! C'est plus petit !";
+  }
+});
+
+//faire le compteur
